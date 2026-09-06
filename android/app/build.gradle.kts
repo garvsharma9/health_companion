a798bd2efe60620d76d7dd5d11d7955e4bd5bc58
Gurmeet <gurmeetgodia@gmail.com>
2026-09-06 22:06:51 +0530

    chore: update gradle configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.healthcompanion.health_companion"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
